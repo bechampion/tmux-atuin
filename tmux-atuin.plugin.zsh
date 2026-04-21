1c9b30c13250732b6e2d05ebc2f6a9d85e074e67
Jeronimo Garcia <garciaj.uk@gmail.com>
2026-04-21 13:11:10 +0100

    fix: restore default up/down arrow history bindings

--- tmux-atuin.plugin.zsh
+++ tmux-atuin.plugin.zsh
@@ -6,3 +6,9 @@ eval "$(atuin init zsh)"
 
 # atuin init sets ATUIN_TMUX_POPUP=false by default; force-enable popup mode.
 export ATUIN_TMUX_POPUP=true
+
+# Keep normal shell history navigation on arrow keys (disable Atuin on Up/Down).
+bindkey '^[[A' up-line-or-history
+bindkey '^[OA' up-line-or-history
+bindkey '^[[B' down-line-or-history
+bindkey '^[OB' down-line-or-history
